1f65c1a9d7c3c83df5d7cc05b8df77972121baac
tormalac <tormalac@gmail.com>
2026-04-09 10:43:29 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- fx-engine.js
+++ fx-engine.js
@@ -16,7 +16,7 @@ fxStyles.innerHTML = `
     #fx-modal-overlay {
         position: fixed; 
         top: 0; bottom: 0; left: 0; right: 0;
-        background: rgba(0,0,0,0.85); z-index: 6000;
+        background: rgba(0,0,0,0.85); z-index: 5000;
         display: none; 
         align-items: flex-start; 
         justify-content: center;
